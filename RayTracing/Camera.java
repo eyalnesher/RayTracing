40910472866b5033c9dbf395f3e7585bff0fa85d
eyalnesher <eyal.nesher@gmail.com>
2021-04-20 16:16:38 +0300

    Fix the up vector of a given camera

--- RayTracing/Camera.java
+++ RayTracing/Camera.java
@@ -1,5 +1,8 @@
 package RayTracing;
 
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 /**
  * An implementation of the camera, containing the camera parameters.
  */
@@ -10,11 +13,24 @@ public class Camera {
     public final double screenDist;
     public final double screenWidth;
 
-    public Camera(Vector pos, Vector lookAt, Vector up, double screenDist, double screenWidth) {
-        this.position = pos;
+    public Camera(Vector position, Vector lookAt, Vector up, double screenDist, double screenWidth) {
+        this.position = position;
         this.lookAt = lookAt;
-        this.upVector = up;
+        this.upVector = fixUpVector(up, lookAt);
         this.screenDist = screenDist;
         this.screenWidth = screenWidth;
+    }
+
+    /**
+     * Fix the up vector to be perpendicular to the looking direction of the camera
+     * 
+     * @param up     The previous up vector
+     * @param lookAt The direction the camera is looking at
+     * @return A vector perpendicular to `lookAt` with roughly the same direction as
+     *         `up`
+     */
+    private static Vector fixUpVector(Vector up, Vector lookAt) {
+        Vector fixed = up.cross(lookAt).cross(lookAt);
+        return Stream.of(fixed, fixed.neg()).min((u, v) -> up.compareDistances(u, v)).get();
     }
 }
